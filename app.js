c1278554f13a60dfe4732efe157c9abca146c421
arjun-raj1523 <arjraj1523@gmail.com>
2015-05-14 19:33:21 +0530

    With Modules

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-function placeAnOrder(orderNumber){
-
-    console.log("Order Number"+orderNumber);
-    deliver(function(){
-        console.log("Done");
-    });
-}
-//simulate a 5 second operation
-function deliver(callBack){
-    setTimeout(callBack,3000);
-}
-//Place Order
-placeAnOrder(1);
-placeAnOrder(2);
-placeAnOrder(3);
-placeAnOrder(4);
-placeAnOrder(5);
-placeAnOrder(6);
+var movies=require('./movies');
+movies.func1();
+movies.func2();
---
+++ movies.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Arjun-Admin on 14-05-2015.
+ */
+function function1(){
+
+    console.log("Works in function1");
+
+}
+
+function function2(){
+    console.log("Works in function2");
+
+}
+
+module.exports.func1= function1;
+module.exports.func2= function2;
