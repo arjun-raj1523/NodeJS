d801bfe139b8ff499f4cc81503cca21afebc9eb8
arjun-raj1523 <arjraj1523@gmail.com>
2015-05-16 08:07:47 +0530

    Server Functionallity with index.html

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var http= require('http');
+var fs= require('fs');
 
-function onRequest(request,response){
 
-    console.log('User made request'+request.url);
+//404 Responce
+function sentOnResponce(response){
     response.writeHead(200,{"Context-Type":"text/plain"});
-    response.write("Hello World");
+    response.write("WebPage NOT Found");
     response.end();
+
+}
+//User Request
+function onRequest(request,response){
+    if( request.method == 'GET' && request.url=='/'){
+        console.log('Request Made');
+        response.writeHead(200,{"Context-Type":"text/html"});
+        fs.createReadStream("./public/stylesheets/index.html").pipe(response);
+
+    }else{
+        sentOnResponce(response);
+    }
 }
 
 http.createServer(onRequest).listen(8080);
